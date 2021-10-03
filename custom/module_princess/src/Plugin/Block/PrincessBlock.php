1638d939b1d5997098cd2e17b76b0e875a14b75d
Viola Lin <shuguann@gmail.com>
2021-10-03 15:32:35 -0700

    Renderable array example in block

--- custom/module_princess/src/Plugin/Block/PrincessBlock.php
+++ custom/module_princess/src/Plugin/Block/PrincessBlock.php
@@ -18,9 +18,45 @@ class PrincessBlock extends BlockBase {
     * {@inheritdoc}
     */
     public function build(){
-         return [
-             '#type'=>'markup',
-             '#markup'=>$this->t('This is the output of our princess block '),
-        ]; 
+        $princess = [
+		 
+            ['name' => 'Snow White' , 'Film'=>'Snow White and the Seven Dwarfs '],
+            ['name' => 'Cinderella', 'Film'=>' Cinderella'],
+            ['name' => 'Aurora', 'Film'=>'Sleeping Beauty'],
+            ['name' => 'Ariel', 'Film'=>'The Little Mermaid '],
+            ['name' => 'Belle', 'Film'=>'Beauty and the Beast '],
+            ['name' => 'Jasmine', 'Film'=>'Aladdin'],
+            ['name' => 'Pocahontas', 'Film'=>'Pocahontas'],
+            ['name' => 'Mulan', 'Film'=>'Mulan'],
+            ['name' => 'Tiana', 'Film'=>'The Princess and the Frog '],
+            ['name' => 'Rapunzel', 'Film'=>'Tangled '],
+            ['name' => 'Merida', 'Film'=>'Brave'],
+            ['name' => 'Moana', 'Film'=>'Moana '],
+            ['name' => 'Raya', 'Film'=>'Raya']
+          ];
+
+        $table = [
+            '#type' => 'table',
+            '#header' =>[
+                $this->t('Princess Name'),
+                $this->t('Film Name'),
+            ]
+        ];
+
+
+        foreach($princess as $p){
+            $table[] = [
+               'princess_n'=>[
+                   '#type' => 'markup',
+                   '#markup' => $p['name'],
+               ],
+               'film_n' => [
+                   '#type' => 'markup',
+                   '#markup' => $p['Film'],
+               ],
+            ];
+        }
+        return $table;
+
     }
 }
