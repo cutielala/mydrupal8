1e8396e44ac2427114a7d418b2b4a6fda76dcb9a
Viola Lin <shuguann@gmail.com>
2021-10-10 00:42:28 -0700

    custom form #1

---
+++ custom/module_princess/src/Form/PrincessForm.php
@@ -0,0 +1,54 @@
+<?php
+namespace Drupal\module_princess\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+/**
+ * Provides a configuration form for configurable actions.
+ *
+ * @internal
+ */
+class PrincessForm extends FormBase {
+/**
+   * {@inheritdoc}
+   */
+    public function getFormId() {
+    return 'module_princess_form';
+     }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+  
+    $form['c_1'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Candidate 1'),  
+ 
+    ];
+
+    $form['c_2'] = [
+      '#type' => 'textfield',
+      '#title' => $this ->t('Candidate 2'),
+    ];
+
+    $form['submit']= [
+      '#type' => 'submit',
+      '#value' => $this->t('who will win?'),
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+    public function submitForm(array &$form, FormStateInterface $form_state) {
+    $winner = rand(1,2);
+    drupal_set_message(
+        'The winnder between '.$form_state->getValue('c_1').' and '.$form_state->getValue('c_2'). ' is:'. $form_state->getValue('c_'.$winner)
+  
+    
+        );
+    }
+  
+}
