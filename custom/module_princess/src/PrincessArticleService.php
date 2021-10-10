c45e2f65df76c0b4678935911ebaffb3f1940a66
Viola Lin <shuguann@gmail.com>
2021-10-09 18:46:40 -0700

    Services injection example #1

--- custom/module_princess/src/PrincessArticleService.php
+++ custom/module_princess/src/PrincessArticleService.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\module_princess;
 
+use Drupal\Core\Entity\Query\QueryFactory;
 /***
  * our pricess article service class
  */
@@ -10,9 +11,15 @@ namespace Drupal\module_princess;
     /**
      * method for getting articles, regarding princess
      */
+    private $entityQuery;
+    public function __construct(QueryFactory $entityquery){
+        $this -> entityQuery = $entityquery;
+    }
+
      public function getPrincessArticles(){
          $articles = [ '"Snow White" is a 19th-century German fairy tale that is today known widely across the Western world.'  , '“Beauty and the Beast” is the fantastic journey of Belle, a bright, beautiful and independent young woman who is taken prisoner by a beast in his castle.' ];
-
+ 
+         kint($this -> entityQuery);
          return $articles;
      }
       
