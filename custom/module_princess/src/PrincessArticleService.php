036e386cba4c1c458d9e2343dcda9c9bb4d896d9
Viola Lin <shuguann@gmail.com>
2021-10-09 20:16:45 -0700

    Services injection example #4

--- custom/module_princess/src/PrincessArticleService.php
+++ custom/module_princess/src/PrincessArticleService.php
@@ -20,10 +20,10 @@ use Drupal\Core\Entity\EntityManager;
     }
 
      public function getPrincessArticles(){
-         $articles = [ '"Snow White" is a 19th-century German fairy tale that is today known widely across the Western world.'  , '“Beauty and the Beast” is the fantastic journey of Belle, a bright, beautiful and independent young woman who is taken prisoner by a beast in his castle.' ];
+        // $articles = [ '"Snow White" is a 19th-century German fairy tale that is today known widely across the Western world.'  , '“Beauty and the Beast” is the fantastic journey of Belle, a bright, beautiful and independent young woman who is taken prisoner by a beast in his castle.' ];
  
          kint($this->entityQuery->get('node')->condition('type' , 'article')->execute() );
-         kint($this->entityManager);
+         kint($this->entityManager->getStorage('node')->loadMultiple());
          return $articles;
      }
       
