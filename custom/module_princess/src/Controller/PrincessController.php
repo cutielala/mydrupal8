3e991e1a9fb905992c1470b5409d3e553f82c4dc
Viola Lin <shuguann@gmail.com>
2021-10-11 21:42:25 -0700

    Print ConfigForm Title

--- custom/module_princess/src/Controller/PrincessController.php
+++ custom/module_princess/src/Controller/PrincessController.php
@@ -7,23 +7,27 @@ use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\module_princess\PrincessArticleService;
 
+use Drupal\Core\Config\ConfigFactory;
+
 class PrincessController extends ControllerBase {
 	
     private $ArticlePrincessS;
+	protected $configFactory;
 
 	public static function create(ContainerInterface $container) {
 		return new static(
-			$container->get('module_princess.princess_article')
+			$container->get('module_princess.princess_article'),
+			$container->get('config.factory')
 		);
 	}
-	public function __construct(PrincessArticleService $ArticlePrincessS ){
+	public function __construct(PrincessArticleService $ArticlePrincessS , ConfigFactory $configFactory ){
         $this->ArticlePrincessS = $ArticlePrincessS;
+		$this->configFactory = $configFactory;
 
     }
      public function princessList(){
-		 
-
-		kint($this->ArticlePrincessS->getPrincessArticles()); die();
+		//kint($this->ArticlePrincessS->getPrincessArticles()); die();
+		//kint($this->configFactory->get('module_princess.settings')->get('princess_list_title')); die();
 
 		 $princess = [
 		 
@@ -47,7 +51,9 @@ class PrincessController extends ControllerBase {
      return [
 	   '#theme' => 'princess_list',
 	   '#items' => $princess,
-       '#title' => $this -> t('Our princesses have arrived')
+     //  '#title' => $this -> t('Our princesses have arrived')
+
+	 '#title' => $this->configFactory->get('module_princess.settings')->get('princess_list_title'),
 	 ];
 }
 }
